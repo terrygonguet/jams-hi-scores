c6ee25cb46f6e7d0fbd790e74a53b4eb7649f615
Daydreel <daydreel@gmail.com>
2018-08-16 20:20:02 +0200

    Coucou de james
    MDR lol un petit commentaire

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const fs = require('fs')
 fs.writeFileSync("test.json", "{ \"test\":2 }", { flag: "w+" })
 
 console.log(fs.readFileSync("test.json").toString())
+
+//Test d'écriture coucou - James :)
